06771ec01365870a56e6d64c969db3eba8092e71
حسناء 31159874 <UG_31159874@ics.tanta.edu.eg>
2025-04-16 12:33:42 -0700

    #created constrains for course ,lesson ,material ,baseEntity and admin tables

---
+++ Backend/Constraints/CourseConstraints.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Constraints;
+
+public class CourseConstraints : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    { 
+        builder.Property(c => c.Description)
+               .HasMaxLength(250);
+
+        builder.Property(c => c.Price)
+               .IsRequired()
+               .HasMaxLength(10);
+
+        builder.Property(c => c.IsDeleted)
+                .IsRequired();
+
+        builder.Property(c => c.CreatedAt)
+               .IsRequired();
+
+        builder.Property(c => c.ImagePath)
+               .HasMaxLength(250);
+
+        builder.HasMany(c => c.lessons)
+               .WithOne(l => l.Course)
+               .HasForeignKey(l => l.CourseId);
+
+      //  builder.HasOne(c => c.Teacher)
+      //      .WithMany(t => t.courses)
+      //      .HasForeignKey(c => c.TecherId)
+      //      .OnDelete(DeleteBehavior.Restrict);
+    }
+}
