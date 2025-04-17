bc630c7b75f3a1cec5ca93be576b8f6c2b118e83
Youssef-Ebn-Yasser <yh29152@gmail>
2025-04-17 13:17:22 +0200

    WIP: local changes before merging HasnaaHassan

---
+++ Backend/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : AppControllerBase
+    {
+        #region   fields
+        ICommentService commentService;
+        #endregion
+
+        #region   Constructor
+        public CommentController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+        #endregion
+
+
+        #region   Handle function
+
+        [HttpGet("")]
+        public async Task<string> get()
+        {
+            var comment = await commentService.GetAllCommentsAsync();
+
+            return NewResult(comment);
+
+        }
+        #endregion
+    }
+}
