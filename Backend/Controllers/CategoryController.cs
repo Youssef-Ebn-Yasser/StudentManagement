54dbb7df2eeba2dc64af6a26c11a9a1868416ccc
MoHannad1223 <mohanedemam408@gmail.com>
2025-04-29 18:24:00 +0300

    Category Topic

--- Backend/Controllers/CategoryController.cs
+++ Backend/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.DTOs.CategoryDTOOS;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -7,5 +8,40 @@ namespace Backend.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _categoryService.GetByIdAsync(id);
+            return Ok ( result);
+        }
+
+        [HttpPut(" Update {id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateCategoryDto dto)
+        {
+            var result = await _categoryService.UpdateAsync(id, dto);
+            return Ok( result);
+        }
+
+        [HttpDelete(" Delete {id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _categoryService.DeleteAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CreateCategoryDto dto )
+        {
+            var result = await _categoryService.CreateAsync(dto);
+            return Ok(result);
+        }
     }
 }
