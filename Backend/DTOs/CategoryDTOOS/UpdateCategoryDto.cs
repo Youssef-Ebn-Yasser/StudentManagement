2e91d1c1e7483c9a7f88f7130062151317cd9cf4
MoHannad1223 <mohanedemam408@gmail.com>
2025-04-29 18:20:59 +0300

    Category Commits

---
+++ Backend/DTOs/CategoryDTOOS/UpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DTOs.CategoryDTOOS
+{
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
