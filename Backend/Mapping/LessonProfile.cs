0ce734f318d22d09bcbceb5ba4149117ebb1f8b9
حسناء 31159874 <UG_31159874@ics.tanta.edu.eg>
2025-05-01 06:03:32 -0700

    #added LessonProfile for mapping

---
+++ Backend/Mapping/LessonProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Backend.DTOs.LessonDTOs;
+
+namespace Backend.Mapping
+{
+    public class LessonProfile : Profile
+    {
+        public LessonProfile()
+        {
+            CreateMap<CreateLessonDto , Lesson>()
+                .ForMember(dest => dest.CourseId, opt => opt.MapFrom(src=>src.CourseId))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+
+        }
+       
+
+    }
+}
