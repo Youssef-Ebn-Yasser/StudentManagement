093d317535a8896c63a0a49dc4dd3393b69565e7
RenadAlshooni <renadalshooni@gmail.com>
2025-05-04 19:16:07 +0300

    Adding Hours to Course

---
+++ Backend/Mapping/MaterialProfile.cs
@@ -0,0 +1,13 @@
+﻿using Backend.DTOs.MaterialDTOs;
+using AutoMapper;
+namespace Backend.Mapping
+{
+    public class MaterialProfile : Profile
+    {
+        public MaterialProfile()
+        {
+
+        }
+    
+    }
+}
