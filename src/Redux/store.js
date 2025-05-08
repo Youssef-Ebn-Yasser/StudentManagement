5e34e976758f372c2827b553ed127b32f4b76557
Hussien Alaa <arelalasey1978@gmail.com>
2025-05-08 17:59:23 +0300

    Revert "Merge branch 'frontend' of https://github.com/Youssef-Ebn-Yasser/StudentManagement into frontend"
    This reverts commit a9bdc326488d4a1d69acf761b9aa10044384cf44, reversing
    changes made to bbb4c5f46273f1ace58b75ac7fab8c63c972abb4.

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,12 +3,6 @@ import authSlice from '@/redux/auth/authSlice';
 import confirmEmailReducer from "@/redux/features/confirmEmail/confirmSlice";
 import loginReducer from "@/redux/features/login/loginSlice";
 // import userDetailsReducer from './features/getUserDetails/userDetailsSlice';
-import allStudentReducer from './features/allStudents/allStudents'
-import allTeacherReducer from './features/allTeachers/allTeachers'
-import allCoursesReducer from './features/allCourses/allCourses'
-import allLessonsReducer from './features/allLessons/allLessons'
-import allGategoryReducer from './features/allGategory/allGategory'
-
 
 let store = configureStore({
     reducer: {
@@ -20,12 +14,6 @@ let store = configureStore({
         // expiration:expiredReducer,
         auth: authSlice,
         // userDetails: userDetailsReducer,
-        allStudents:allStudentReducer,
-        allTeachers:allTeacherReducer,
-        allCourses:allCoursesReducer,
-        allLessons:allLessonsReducer,
-        allGategory:allGategoryReducer
-
     },
 })
 export default store
--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -11,6 +11,8 @@ import TeacherCourses from '@/component/TeacherProfile/TeacherCourses'
 import AccountSettings from '@/component/TeacherProfile/settingsPage/AccountSettings'
 import TeacherCourseDetails from '@/component/TeacherProfile/TeacherCourseDetails'
 import CoursesDetails from '@/component/CoursesDetails/CoursesDetails'
+import AddLesson from '@/component/TeacherProfile/AddLesson'
+import EditCourse from '@/component/TeacherProfile/EditCourse'
 
 const isAuth = true // Replace with actual authentication logic
 
@@ -79,6 +81,12 @@ const routesConfig = [
     {
         path: 'teacher/settings',
         element: <AccountSettings />,
+        isProtected: false,
+        accessRole: 'all',
+    },
+    {
+        path: 'teacher/course/edit/:id',
+        element: <EditCourse />,
         isProtected: false,
         accessRole: 'all',
     },
