12e899a5dec694d0e2d2c6b01633dd9b2dfb1565
Youssef-Ebn-Yasser <yh29152@gmail>
2025-05-09 15:35:50 +0300

    save some fix

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -52,7 +52,8 @@ builder.Services.AddCors(Options =>
        {
            policy.AllowAnyHeader();
            policy.AllowAnyMethod();
-           policy.WithOrigins("http://localhost:5175", "http://127.0.0.1:5500", "http://localhost:5173", "http://localhost:5174");
+           policy.WithOrigins("http://localhost:5175", "https://localhost:5178",
+           "http://127.0.0.1:5500", "http://localhost:5173", "http://localhost:5174");
        });
 });
 #endregion
