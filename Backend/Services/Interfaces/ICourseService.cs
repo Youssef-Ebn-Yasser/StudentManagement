9df872c8c5ec7a11b10875b5737abd02dca2d2bf
RenadAlshooni <renadalshooni@gmail.com>
2025-05-23 20:48:57 +0300

    editing material , courses , category

--- Backend/Services/Interfaces/ICourseService.cs
+++ Backend/Services/Interfaces/ICourseService.cs
@@ -13,6 +13,8 @@ public interface ICourseService
     public Task<Response<string>> UpdateAsync(UpdateCourseDto createCourseDto);
     public Task<Response<string>> DeleteAsync(int id);
     public Task<Response<List<ShowCourseInfoByCategoryDto>>> GetCourseInfoByCategoryAsync(string category);
+
+    public Task<Response<List<ShowCourseDto>>> GetAllCoursesOfTeacherAsync(int teacherId);
 }
 
 public enum enOrderBy { noOrder = 0, Price = 1, CreatedAt = 2, }
