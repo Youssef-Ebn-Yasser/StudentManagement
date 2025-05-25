d8e30fab3fbab168f6a524581dea7f1a99d940d6
Youssef-Ebn-Yasser <yh29152@gmail>
2025-05-25 21:54:00 +0300

    save update - student

--- Backend/Entities/Student.cs
+++ Backend/Entities/Student.cs
@@ -2,6 +2,7 @@
 public class Student : User
 {
     public string? ImageUrl { get; set; }
+    public string? Phone { get; set; }
     public List<StudentCourse>? StudentCourses { get; set; }
     public List<Payment>? Payments { get; set; }
     public List<Comment>? Comments { get; set; }
