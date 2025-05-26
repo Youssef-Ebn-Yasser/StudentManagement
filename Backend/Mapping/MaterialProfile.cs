3fdd8b78523afe0fb63a5113be2c2c36b0094dda
RenadAlshooni <renadalshooni@gmail.com>
2025-05-26 19:25:07 +0300

    ..

--- Backend/Mapping/MaterialProfile.cs
+++ Backend/Mapping/MaterialProfile.cs
@@ -3,18 +3,14 @@ namespace Backend.Mapping;
 
 public class MaterialProfile : Profile
 {
-    public MaterialProfile()
-    {
-<<<<<<< HEAD
+   
         public MaterialProfile()
         {
             CreateMap<Material, ShowMaterialDto>()
                 .ForMember(m => m.Data, ma => ma.MapFrom(opt => opt.Path));
-=======
-        CreateMap<Material, ShowMaterialDto>();
-        CreateMap<CreateMaterialDto, Material>();
-        CreateMap<UpdateMaterialDto, Material>();
->>>>>>> 6f6a458e4c59219f3008cf5ed765a016fb6b85fb
+           
+            CreateMap<CreateMaterialDto, Material>();
+            CreateMap<UpdateMaterialDto, Material>();
 
-    }
+        }
 }
