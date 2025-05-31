d85f9c381f15936bd0b70f9c8e012a5acff8d85e
Hussien Alaa <arelalasey1978@gmail.com>
2025-05-31 19:55:53 +0300

    Updates

--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -36,13 +36,7 @@ import TeacherDashboard from '@/component/TeacherDashboard/TeacherDashboard'
 import LessonDetails from '@/component/StudentDashboard/LessonDetails'
 import CreateZoom from '@/component/TeacherProfile/CreateZoom'
 import MeetingData from '@/component/TeacherProfile/MeetingData'
-import MeetingSdata from './../component/StudentDashboard/MeetingSdata';
-import TeacherProfileView from '@/component/StudentDashboard/TeacherProfileView.jsx'
-import CreateQuiz from '../component/CreateQuiz/CreateQuiz'
-import LessonDetails from '../component/StudentDashboard/LessonDetails'
-
-
-
+import MeetingSdata from './../component/StudentDashboard/MeetingSdata'
 
 const routesConfig = [
     { path: '', element: <Home />, isProtected: false, accessRole: 'all' },
