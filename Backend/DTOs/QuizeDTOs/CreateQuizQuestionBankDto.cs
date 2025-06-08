01eddb767b03980ca05b6f3300b42b2e840bc591
RenadAlshooni <renadalshooni@gmail.com>
2025-06-08 21:46:15 +0300

    ..

---
+++ Backend/DTOs/QuizeDTOs/CreateQuizQuestionBankDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.DTOs.QuizeDTOs
+{
+    public class CreateQuizQuestionBankDto
+    {
+        public int CourseId { get; set; }
+        public List<QuestionListDto> QuestionListDtos { get; set; } = new();
+    }
+}
