c0fa5e50433c8892010d47b2767b2c84b99cc016
RenadAlshooni <renadalshooni@gmail.com>
2025-06-11 00:17:20 +0300

    ..

--- Backend/Services/Implementation/QuizService.cs
+++ Backend/Services/Implementation/QuizService.cs
@@ -1,72 +1,77 @@
-﻿//using Backend.DTOs.QuizeDTOs;
-//using Backend.Entities.QuizeEntities;
-//using Microsoft.EntityFrameworkCore;
+﻿using Backend.DTOs.QuizeDTOs;
+using Backend.Entities.QuizeEntities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Backend.Services.Implementation
-//{
-//    public class QuizService : IQuizService
-//    {
+namespace Backend.Services.Implementation
+{
+    public class QuizService : IQuizService
+    {
 
-//        private readonly IUnitOfWork _unitOfWork;
-//        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-//        public QuizService(IUnitOfWork unitOfWork, IMapper mapper)
-//        {
-//            _unitOfWork = unitOfWork;
-//            _mapper = mapper;
-//        }
+        public QuizService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
 
-//        public void CorrectQuiz(CorrectQuizDto dto)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void CorrectQuiz(CorrectQuizDto dto)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public List<QuizToCorrectDto> GetQuizzesToCorrectByLessonId(int lessonId)
-//        {
+        public List<QuizToCorrectDto> GetQuizzesToCorrectByLessonId(int lessonId)
+        {
 
-//            var quizzes = _unitOfWork.Repository<StudentQuizeAnswer>()
-//            .GetTableNoTracking()
-//            .Where(q => q.Quiz.LessonId == lessonId)
-//            .Where(sqa => sqa.StudentQuestionAnswer.IsCorrect == null)
-//            .Select(sqa => new QuizToCorrectDto
-//            {
-//                StudentQuizAnswerId = sqa.Id,
-//                QuizName = sqa.Quiz.Title,
-//                StudentName = sqa.Student.Name,
+            var quizzes = _unitOfWork.Repository<StudentQuizeAnswer>()
+            .GetTableNoTracking()
+            .Where(q => q.Quiz.LessonId == lessonId)
+            .Where(sqa => sqa.StudentQuestionAnswer.IsCorrect == null)
+            .Select(sqa => new QuizToCorrectDto
+            {
+                StudentQuizAnswerId = sqa.Id,
+                QuizName = sqa.Quiz.Title,
+                StudentName = sqa.Student.Name,
 
-//            })
-//            .ToList();
+            })
+            .ToList();
 
-//            return quizzes;
-//        }
+            return quizzes;
+        }
 
-//        public StudentQuizAnswerDto GetStudentQuizAnswer(int answerId)
-//        {
+        public List<StudentQuizAnswerDto> GetStudentQuizAnswer(int answerId)
+        {
 
-//            return _unitOfWork.Repository<StudentQuestionAnswer>()
-//             .GetTableNoTracking()
-//             .Where(a => a.studentQuizeAnswerId == answerId && a.IsCorrect == null)
-//             .Select(a => new StudentQuizAnswerDto
-//             {
-//                 Answer = a.studentQuestionOptions.Select(o => o.QuestionOptionId).ToList(),
-//                 IsCorrect = null
+            return _unitOfWork.Repository<StudentQuestionAnswer>()
+             .GetTableNoTracking()
+             .Where(a => a.studentQuizeAnswerId == answerId && a.IsCorrect == null)
+             .Include(a => a.studentQuestionOptions)
+             .ThenInclude(opt => opt.QuestionOption)
+             .Select(a => new StudentQuizAnswerDto
+             {
+                 Answer = a.studentQuestionOptions.Select(opt=> opt.QuestionOption.OptionText).ToList(),
+                 IsCorrect = null
 
-//             })
-//             .ToList();
-//        }
+             })
+             .ToList();
+        }
 
-//        public void CorrectQuiz(int AnswerId, bool isCorrect)
-//        {
-//            var answer = _unitOfWork.Repository<StudentQuestionAnswer>()
-//                .GetTableNoTracking()
-//                .Where(a => a.Id == AnswerId).FirstOrDefault();
+        public void CorrectQuiz(int AnswerId, bool isCorrect)
+        {
+            var answer = _unitOfWork.Repository<StudentQuestionAnswer>()
+                .GetTableAsTracking()
+                .Where(a => a.Id == AnswerId).FirstOrDefault();
 
-//            if (answer == null) 
-//                throw new Exception("Answer not found");
-//            answer.IsCorrect = isCorrect;
 
-//            _unitOfWork.Repository<StudentQuizeAnswer>().Update(answer);
-//            _unitOfWork.Complete();
-//        }
-//    }
-//}
+            if (answer == null)
+                throw new Exception("Answer not found");
+
+            answer.IsCorrect = isCorrect;
+
+            _unitOfWork.Repository<StudentQuestionAnswer>().Update(answer);
+            _unitOfWork.Complete();
+        }
+    }
+}
