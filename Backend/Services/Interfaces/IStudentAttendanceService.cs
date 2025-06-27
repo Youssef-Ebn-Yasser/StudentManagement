fd669e6725d821f12e7112c1600668bd1a94942d
Youssef-Ebn-Yasser <yh29152@gmail>
2025-06-27 15:15:44 +0300

    save

--- Backend/Services/Interfaces/IStudentAttendanceService.cs
+++ Backend/Services/Interfaces/IStudentAttendanceService.cs
@@ -1,11 +1,10 @@
-﻿
-namespace Backend.Services.Interfaces
+﻿namespace Backend.Services.Interfaces
 {
     using Backend.DTOs.StudentDOs;
     public interface IStudentAttendanceService
     {
-        public Task<Response<string>> MarkAttendanceAsync(int meetingId, List<StudentAttendanceStatusDto> attendances);
-        public Task<Response<List<StudentAttendanceStatusDto>>> GetAttendanceByMeetingAsync(int meetingId);
-        public Task<Response<List<StudentAttendanceStatusDto>>> GetAttendanceByStudentAsync(int studentId);
+        public Task<Response<string>> MarkAttendanceAsync(int meetingId, List<StudentAttendanceDto> attendances);
+        public Task<Response<List<StudentAttendanceDto>>> GetAttendanceByMeetingAsync(int meetingId);
+        public Task<Response<List<StudentAttendanceDto>>> GetAttendanceByStudentAsync(int studentId);
     }
 }
