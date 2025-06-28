c8a3131789c15f39c117166d0786bbf31fb4fcfc
حسناء 31159874 <UG_31159874@ics.tanta.edu.eg>
2025-06-28 21:14:30 +0300

    added slider

---
+++ Backend/DTOs/SliderDTOs/SliderDto.cs
@@ -0,0 +1,9 @@
+namespace Backend.DTOs.SliderDTOs;
+
+public class SliderDto
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+    public string Link { get; set; } = string.Empty;
+} 
---
+++ Backend/Migrations/20250628181351_addSlidersTable.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSlidersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserTypeAr",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserTypeEn",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "StudentAssignments",
+                newName: "NameEn");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameAr",
+                table: "StudentAssignments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameAr",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressAr",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressEn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Admin_NationalId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GovernmentAr",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GovernmentEn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "RandomCode",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContentAr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContentEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentAttendances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    MeetingId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MeetingId1 = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentAttendances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentAttendances_AspNetUsers_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentAttendances_Meetings_MeetingId1",
+                        column: x => x.MeetingId1,
+                        principalTable: "Meetings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentAttendances_MeetingId1",
+                table: "StudentAttendances",
+                column: "MeetingId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentAttendances_StudentId",
+                table: "StudentAttendances",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sliders");
+
+            migrationBuilder.DropTable(
+                name: "StudentAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "NameAr",
+                table: "StudentAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "AddressAr",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressEn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Admin_NationalId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GovernmentAr",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GovernmentEn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RandomCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "NameEn",
+                table: "StudentAssignments",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameAr",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserTypeAr",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserTypeEn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
